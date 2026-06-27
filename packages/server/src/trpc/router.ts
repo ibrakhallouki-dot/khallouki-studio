18f9b5b8af4f84f5be61d5d17e49f1d745e26fcb
ibrakhallouki-dot <ibrakhallouki@gmail.com>
2026-06-27 15:04:47 +0100

    fix(server): normalize and fix trpc root router imports/exports

--- packages/server/src/trpc/router.ts
+++ packages/server/src/trpc/router.ts
@@ -1,11 +1,20 @@
+import { initTRPC } from '@trpc/server'
+import { createTRPCContext } from './context'
+import { designsRouter } from './routers/designs'
+import { resourcesRouter } from './routers/resources'
+import { interactionsRouter } from './routers/interactions'
 import { usersRouter } from './routers/users'
 import { adminRouter } from './routers/admin'
-@@
- export const appRouter = t.router({
-   designs: designsRouter,
-   resources: resourcesRouter,
-   interactions: interactionsRouter,
-+  users: usersRouter,
-+  admin: adminRouter,
-   health: t.procedure.query(() => ({ ok: true }))
- })
+
+const t = initTRPC.context<any>().create()
+
+export const appRouter = t.router({
+  designs: designsRouter,
+  resources: resourcesRouter,
+  interactions: interactionsRouter,
+  users: usersRouter,
+  admin: adminRouter,
+  health: t.procedure.query(() => ({ ok: true }))
+})
+
+export type AppRouter = typeof appRouter
